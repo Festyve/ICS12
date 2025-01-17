7358f9b8a433b7c2aed32da6686ff1f202892a15
Festyve <100452186+Festyve@users.noreply.github.com>
2025-01-16 23:36:43 -0500

    Update VerticalLaserAttack.java

--- VerticalLaserAttack.java
+++ VerticalLaserAttack.java
@@ -1,7 +1,7 @@
 /*
 * Author: Michael Zhang and Bryan Yao
 * Date: 2025-01-16
-* Description: This class implements a  laser attack pattern for the boss character.
+* Description: This class implements a laser attack pattern for the boss character.
 * It manages the spawning, behaviour, and timing of the lasers, that spawn randomly or track the player.
 * The attack also changes based on the game's cycle count.
 */
